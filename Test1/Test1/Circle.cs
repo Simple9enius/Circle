38f681f99b78f5a6f631a2967657ac3d85c4bdd8
igor <sas.ihor.new@gmail.com>
2019-03-28 09:52:08 +0200

    add all project

---
+++ Test1/Test1/Circle.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Circle
+    {
+        private double X{ get; set; }
+        private double Y { get; set; }
+        private double Radius { get; set; }
+
+        public Circle(double x, double y, double radius)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+
+        public static bool operator >(Circle c1, Circle c2)
+        {
+            return c1.Radius > c2.Radius;
+        }
+
+        public static bool operator <(Circle c1, Circle c2)
+        {
+            return c1.Radius < c2.Radius;
+        }
+
+        public static bool operator >=(Circle c1, Circle c2)
+        {
+            return c1.Radius >= c2.Radius;
+        }
+
+        public static bool operator <=(Circle c1, Circle c2)
+        {
+            return c1.Radius <= c2.Radius;
+        }
+
+        public static bool operator ==(Circle c1, Circle c2)
+        {
+            return c1.Radius == c2.Radius;
+        }
+
+        public static bool operator !=(Circle c1, Circle c2)
+        {
+            return c1.Radius != c2.Radius;
+        }
+
+        public double getSquare()
+        {
+            return Math.Pow(Radius, 2) * Math.PI;
+        }
+
+        public double getLength()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public static bool isIntersection(Circle c1, Circle c2) {
+            setCorrectCircles(c1, c2);
+            double d = Math.Sqrt(Math.Pow(c2.X - c1.X, 2) + Math.Pow(c2.Y - c1.Y, 2));
+            if(c1 == c2 && c1.X == c2.X && c1.Y == c2.Y)
+            {
+                Console.WriteLine("There're the same circles");
+                return true;
+            }
+            else if (c1.Radius + c2.Radius > d || d < c1.Radius - c2.Radius)
+            {
+                Console.WriteLine("There aren't intersection point");
+                return false;
+            }
+            else if (c1.Radius + c2.Radius == d || c1.Radius - c2.Radius == d)
+            {
+                Console.WriteLine("There are one intersection point");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("There are two intersection point");
+                return true;
+            }
+        }
+        
+        private static void setCorrectCircles(Circle c1, Circle c2)
+        {
+            if(c1.Radius < c2.Radius)
+            {
+                Circle tmp = c1;
+                c1 = c2;
+                c2 = c1;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "X : " + X + "\nY : " + Y + "\nRadius : " + Radius; 
+        }
+
+        public bool isInFirstSection()
+        {
+            return X >= 0 && Y >= 0;
+        }
+    }
+}
---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input n: ");
+                int n = Int32.Parse(Console.ReadLine());
+                Circle[] circles = new Circle[n];
+                for (int i = 0; i < n; i++)
+                {
+                    double x, y, r;
+                    Console.WriteLine("\nInput x for circle number " + i);
+                    x = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("\nInput y for circle number " + i);
+                    y = Double.Parse(Console.ReadLine());
+                    Console.WriteLine("\nInput radius for circle number " + i);
+                    r = Double.Parse(Console.ReadLine());
+                    if (r < 0)
+                    {
+                        Console.WriteLine("Radius should be positive!");
+                        return;
+                    }
+                    circles[i] = new Circle(x, y, r);
+                }
+                Console.WriteLine("\nStarted circles: ");
+                printCircles(circles);
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (circles[j] > circles[j + 1])
+                        {
+                            Circle tmp = circles[j];
+                            circles[j] = circles[j + 1];
+                            circles[j + 1] = tmp;
+                        }
+                    }
+                }
+                Console.WriteLine("\nCircles after sorting: ");
+                printCircles(circles);
+
+                Console.WriteLine("Input one more circle: ");
+                double x1, y1, r1;
+                Console.WriteLine("\nInput x for circle");
+                x1 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("\nInput y for circle");
+                y1 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("\nInput radius for circle");
+                r1 = Double.Parse(Console.ReadLine());
+                if (r1 < 0)
+                {
+                    Console.WriteLine("Radius should be positive!");
+                    return;
+                }
+                Circle circle = new Circle(x1, y1, r1);
+                int countIntersection = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (Circle.isIntersection(circle, circles[i]))
+                    {
+                        Console.WriteLine("\nYour circle has intersectin with circle number " + i);
+                        countIntersection++;
+                    }
+                }
+                Console.WriteLine("\nSummary count intersection " + countIntersection);
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (circles[i].isInFirstSection())
+                    {
+                        Console.WriteLine("Length of circle number " + i + "= " + circles[i].getLength());
+                    }
+                }
+            }catch(Exception e)
+            {
+                Console.WriteLine("Something wrong! Check your input!");
+            }
+            Console.ReadLine();
+        }
+
+        private static void printCircles(Circle[] circles)
+        {
+            for (int i = 0; i < circles.Length; i++)
+            {
+                Console.WriteLine("\nCircle number " + i + "\n" + circles[i]);
+            }
+        }
+
+    }
+}
